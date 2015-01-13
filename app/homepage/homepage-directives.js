a6c1019a595b4b55826b0bd49ad43241af726e6d
Jekt <jekt.tl@gmail.com>
2015-01-13 15:15:39 +0100

    Homepage basics: article list with titles

---
+++ app/homepage/homepage-directives.js
@@ -0,0 +1,31 @@
+(function(){
+	var app = angular.module('homepage-directives', []);
+
+	app.directive('homepageList', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'homepage/homepage-list.html',
+			controller: ['$http', function($http){
+				var hp = this;
+				hp.articleList = [];
+				hp.currentArticle = 0;
+
+				$http.get('homepage/simulation-hp.json')
+					.success(function(data){
+						hp.articleList = data.posts;
+					});
+
+				hp.isShown = function(articleID){
+					return hp.currentArticle === articleID;
+				};
+
+				hp.showArticle = function(articleID){
+					hp.currentArticle = !hp.isShown(articleID) ?
+										hp.currentArticle = articleID :
+										hp.currentArticle = 0;
+				};
+			}],
+			controllerAs: 'hp'
+		};
+	})
+})();
